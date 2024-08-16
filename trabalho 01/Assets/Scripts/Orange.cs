e1f8f727170b3038f9179ba4ea299a9975d96eac
Flávia Cunha <cunha.flavia@escolar.ifrn.edu.br>
2024-08-16 10:16:31 -0300

    Aula 08 (Metade)

--- trabalho 01/Assets/Scripts/Orange.cs
+++ trabalho 01/Assets/Scripts/Orange.cs
@@ -9,6 +9,7 @@ public class Orange : MonoBehaviour
     private CircleCollider2D circle;
 
     public GameObject collected;
+    public int Score;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +25,10 @@ public class Orange : MonoBehaviour
             sr.enabled = false;
             circle.enabled = false;
             collected.SetActive(true);
+
+            GameController.instance.totalScore += Score;
             
-            Destroy(gameObject, 0.5f);
+            Destroy(gameObject, 0.25f);
             
         }
     }
